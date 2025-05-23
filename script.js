7824b3844a8ff4cf6a8d7ae098d195484b4158ad
Pablo <pablogroza2000@gmail.com>
2025-05-23 14:06:28 +0200

    Seguimos trabajando con css y javascript

--- script.js
+++ script.js
@@ -4,10 +4,8 @@ const closeButton = document.querySelector("#menu-close-button");
 
 // Abrir menú móvil
 openButton.addEventListener("click", () => {
-  document.body.classList.add("show-mobile-menu");
+  document.body.classList.toggle("show-mobile-menu");
 });
 
 // Cerrar menú móvil
-closeButton.addEventListener("click", () => {
-  document.body.classList.remove("show-mobile-menu");
-});
+closeButton.addEventListener("click", () => openButton.click());
